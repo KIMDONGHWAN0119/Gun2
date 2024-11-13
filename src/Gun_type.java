b0763e709f7a69f7ca6beb5baceb45b9e8df47fa
DONGHWAN <mpqd2005@gmail.com>
2024-11-13 13:12:06 +0900

    First

---
+++ src/Gun_type.java
@@ -0,0 +1,12 @@
+
+public class Gun_type extends Gun {
+
+    void shotgun(int i) {
+        valueChange(i,15, 5, 50);
+    }
+
+    void rifle(int i) {
+        valueChange(i,80,30,10);
+    }
+
+}
